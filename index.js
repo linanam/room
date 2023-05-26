33c9d3381efa9d05a8414486269bd37fe68873d3
linanam <galina.stankevich@ewave.com>
2023-05-26 12:51:44 +0200

    Add files

---
+++ index.js
@@ -0,0 +1,35 @@
+(function (){
+    let slideIndex = 1;
+
+    window.addEventListener("load", () => {
+        let slideNavigation = document.getElementById('slideNavigation');
+        slideNavigation.addEventListener("click", (e) => {
+            let i = 1;
+            if (e.target.classList.contains('prev')) {
+                i = -1;
+            }
+            nextSlide(i);
+        });
+
+        showSlides(slideIndex);
+    });
+
+    function nextSlide(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function showSlides(n) {
+        let i,
+            slides = document.getElementsByClassName("slide");
+        if (n > slides.length) {
+            slideIndex = 1
+        }
+        if (n < 1) {
+            slideIndex = slides.length
+        }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].classList.remove("active");
+        }
+        slides[slideIndex - 1].classList.add("active");
+    }
+})()
